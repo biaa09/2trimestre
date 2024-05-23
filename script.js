8cc69b8bb7404784bcff5aabef9f0a2bc06806c7
biaa09 <110111903+biaa09@users.noreply.github.com>
2024-05-23 12:21:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = Document.querySelector(".caixa-principal");
+const caixaPerguntas = Document.querySelector(".caixa-perguntas");
+const caixaAlternativas = Document.querySelector(".caixa-alternativas");
+const caixaResultado = Document.querySelector(".caixa-resultado");
+const textoResultado = Document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+  enunciado: "Pergunta 1", 
+  alternativas:[
+    "Alternativa 1", 
+  "Alternativa 2"
+    ]
+    },
+   "Pergunta 2"
+]
